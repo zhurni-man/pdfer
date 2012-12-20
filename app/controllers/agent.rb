d619a1a6eefb4f75d10cec16c1da21455b75b2e8
jsagisi <jsagisi@propertyline.com>
2012-12-20 11:56:01 -0800

    commented cleanup

--- app/controllers/agent.rb
+++ app/controllers/agent.rb
@@ -1,22 +1,5 @@
 Pdfupper.controllers :agent do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
-
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
+ 
   get :search do
     @results = Agent.search_for("#{params[:search]}", :order => 'name_last asc')
     render 'agent/search'
@@ -27,7 +10,6 @@ Pdfupper.controllers :agent do
     session['agent_id'] = @agent.id
 
     @uploads = Upload.where(agent_id: @agent.id).sort({created_at: -1})
-    #@listings = Listing.find(:all, :conditions => [ "agent_id = ?", @agent.id], :limit => 5)
     render 'agent/show'
   end
 
--- app/controllers/upload.rb
+++ app/controllers/upload.rb
@@ -1,22 +1,4 @@
 Pdfupper.controllers :upload do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
-
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
 
   get :index do
     @uploads = Upload.all
@@ -34,32 +16,15 @@ Pdfupper.controllers :upload do
     end
     if params[:file]
       uploaded_pdf = params[:file].to_s
-      
-      #@img_path = "images/pdfprocess/#{@agent.id}/thumbs/#{File.basename(uploaded_pdf, '.pdf')}_*.jpg"
       if params[:page]
-        #Docsplit.extract_images(uploaded_pdf, :size => '1500x', :format => :jpg, :output => "/mnt/pdfprocess/#{@agent.id}/thumbs/")
-        #@imgs = Dir.glob("/mnt/pdfprocess/#{@agent.id}/thumbs/#{File.basename(uploaded_pdf, '.pdf')}_*.jpg")
         @uploaded_pdf = uploaded_pdf
       elsif params[:img]
         @extracted_imgs = Dir.glob("/mnt/pdfprocess/#{@agent.id}/images/#{@upload.id}-*.jpg")
-        # DO THAT CONVERTING THING HERE, REMEMBER
       else
         Docsplit.extract_text(uploaded_pdf, :ocr => false, :output => "/mnt/pdfprocess/#{@agent.id}/")
         fileline = File.readlines(uploaded_pdf.gsub(/\.[^.]*$/, ".txt"))
         @s = fileline.reject {|x| x == "\n" || x == "\f" }
       end
-      #@s.reject{ |e| e.include?("\n")}
-      #myfile = File.open(uploaded_pdf.gsub(/\.[^.]*$/, ".txt"))
-      #@s.gsub!
-      #file_text = rename_file_extension(uploaded_pdf, "txt")
-      #@s = "#{file_text}"
-      #reader = PDF::Reader.new(uploaded_pdf)
-      #count = 1
-      #@pdfdata = reader.page(count).text
-      #@datalist = []
-      #pdfdata.each do |data|
-        #@datalist.push(data)
-      #end
     end
     render 'upload/show'
   end
@@ -80,12 +45,7 @@ Pdfupper.controllers :upload do
       unless File.exists? File.expand_path(user_img_dir)
         FileUtils.mkdir_p(user_img_dir)
       end
-      #bname = File.basename(unique(params['myfile'][:filename]), ".pdf")
-      #file_dir = user_dir + bname + '/'
       extname = File.extname(params['myfile'][:filename])
-      #unless File.exists? File.expand_path(file_dir)
-      #  Dir.mkdir(file_dir, 0777)
-      #end
       if extname == ".pdf"
         unique_file = unique(user_dir + params['myfile'][:filename])
         File.open(unique_file, "w") do |f|
